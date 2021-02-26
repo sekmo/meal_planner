d1bccb557e0f79e17a348c644b94fde52de49a1a
sekmo <francescojjmari@gmail.com>
2021-02-27 00:18:32 +0100

    Add kind to Ingredient

--- lib/meal_planner/ingredient.rb
+++ lib/meal_planner/ingredient.rb
@@ -1,8 +1,9 @@
 module MealPlanner
   class Ingredient
-    attr_reader :name, :quantity_for_lunch, :quantity_for_dinner
-    def initialize(name: , quantity_for_lunch:, quantity_for_dinner:)
+    attr_reader :name, :kind, :quantity_for_lunch, :quantity_for_dinner
+    def initialize(name:, kind:, quantity_for_lunch:, quantity_for_dinner:)
       @name = name
+      @kind = kind
       @quantity_for_lunch = quantity_for_lunch
       @quantity_for_dinner = quantity_for_dinner
     end
--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -3,9 +3,10 @@ require_relative "test_helper"
 
 class IngredientTest < MiniTest::Test
   def test_should_be_initialized_with_name_and_quantities
-    ingredient = MealPlanner::Ingredient.new(name: "Salmon", quantity_for_lunch: 100, quantity_for_dinner: 80)
+    ingredient = MealPlanner::Ingredient.new(name: "Salmon", kind: "protein", quantity_for_lunch: 100, quantity_for_dinner: 80)
     assert_equal("Salmon", ingredient.name)
     assert_equal(100, ingredient.quantity_for_lunch)
     assert_equal(80, ingredient.quantity_for_dinner)
+    assert_equal("protein", ingredient.kind)
   end
 end
