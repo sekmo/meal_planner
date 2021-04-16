867a108fc66803c40656f809b75a55fc0fd6bf29
sekmo <francescojjmari@gmail.com>
2021-04-16 23:16:13 +0200

    Add quantity_for_proteinfree_dinner to Ingredient

--- lib/meal_planner/ingredient.rb
+++ lib/meal_planner/ingredient.rb
@@ -1,11 +1,12 @@
 module MealPlanner
   class Ingredient
-    attr_reader :name, :kind, :quantity_for_lunch, :quantity_for_dinner
-    def initialize(name:, kind:, quantity_for_lunch: nil, quantity_for_dinner: nil)
+    attr_reader :name, :kind, :quantity_for_lunch, :quantity_for_dinner, :quantity_for_proteinfree_dinner
+    def initialize(name:, kind:, quantity_for_lunch: nil, quantity_for_dinner: nil, quantity_for_proteinfree_dinner: nil)
       @name = name
       @kind = kind
       @quantity_for_lunch = quantity_for_lunch
       @quantity_for_dinner = quantity_for_dinner
+      @quantity_for_proteinfree_dinner = quantity_for_proteinfree_dinner
     end
 
     def ==(other_ingredient)
--- test/ingredient_book_test.rb
+++ test/ingredient_book_test.rb
@@ -10,20 +10,20 @@ class MealPlanner::IngredientBookTest < MiniTest::Test
     assert_equal("Bread", carb.name)
     assert_equal("carb", carb.kind)
     assert_equal(30, carb.quantity_for_lunch)
-    assert_equal(80, carb.quantity_for_dinner)
+    assert_equal(30, carb.quantity_for_dinner)
 
     protein = ingredients_book.proteins.first
     assert_equal(MealPlanner::Ingredient, protein.class)
     assert_equal("Chicken", protein.name)
     assert_equal("protein", protein.kind)
     assert_equal(180, protein.quantity_for_lunch)
-    assert_equal(100, protein.quantity_for_dinner)
+    assert_equal(180, protein.quantity_for_dinner)
 
     veggie = ingredients_book.veggies.first
     assert_equal(MealPlanner::Ingredient, veggie.class)
     assert_equal("Lettuce", veggie.name)
     assert_equal("veggie", veggie.kind)
-    assert_equal(57, veggie.quantity_for_lunch)
-    assert_equal(100, veggie.quantity_for_dinner)
+    assert_equal(300, veggie.quantity_for_lunch)
+    assert_equal(300, veggie.quantity_for_dinner)
   end
 end
