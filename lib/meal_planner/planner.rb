9a150df053a9e691bac9ec1ec020ccccb3e5048f
sekmo <francescojjmari@gmail.com>
2021-05-01 13:24:06 +0200

    Refactor Planner#generate_plan

--- lib/meal_planner/planner.rb
+++ lib/meal_planner/planner.rb
@@ -1,31 +1,38 @@
 module MealPlanner
   class Planner
     MENU_CHARS_LENGTH = 55
-    DAYS_NAMES = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    PROTEINFREE_DINNER_DAYS = ["Tuesday", "Thursday", "Saturday", "Sunday"]
 
-    attr_reader :ingredient_book, :max_weekly_frequency_ingredient
+    attr_reader :ingredient_sampler
     attr_accessor :meals
     
     def initialize(ingredient_book, max_weekly_frequency_ingredient)
-      @ingredient_book = ingredient_book
-      @max_weekly_frequency_ingredient = max_weekly_frequency_ingredient
+      @ingredient_sampler = IngredientSampler.new(ingredient_book, max_weekly_frequency_ingredient)
       @meals = []
     end
 
     def generate_plan
-      ingredient_sampler = IngredientSampler.new(ingredient_book, max_weekly_frequency_ingredient)
-      7.times do |index|
-        meals << MealSampler.generate(
-          meals,
-          ingredient_sampler,
-          meal_type: :lunch
-        )
-        meals << MealSampler.generate(
-          meals,
-          ingredient_sampler,
-          meal_type: index.even? ? :dinner : :proteinfree_dinner
-        )
+      DAYS.each do |day|
+        meals << generate_lunch
+        meals << generate_dinner(proteinfree: PROTEINFREE_DINNER_DAYS.include?(day))
       end
+    end
+
+    def generate_lunch
+      MealSampler.generate(
+        meals,
+        ingredient_sampler,
+        meal_type: :lunch
+      )
+    end
+
+    def generate_dinner(proteinfree: false)
+      MealSampler.generate(
+        meals,
+        ingredient_sampler,
+        meal_type: (proteinfree ? :proteinfree_dinner : :dinner)
+      )
     end
 
     def print
@@ -33,9 +40,8 @@ module MealPlanner
       puts "WEEKLY PLAN".center(MENU_CHARS_LENGTH)
       puts "=" * MENU_CHARS_LENGTH
       meals.each_slice(2).with_index do |meals_for_the_day, index|
-        puts "#{DAYS_NAMES[index]} lunch: #{meals_for_the_day[0].to_menu_item}"
-        puts "#{DAYS_NAMES[index]} dinner: #{meals_for_the_day[1].to_menu_item}"
-        # puts "- dinner: #{meals_for_the_day[1].to_menu_item(:dinner)}"
+        puts "#{DAYS[index]} lunch: #{meals_for_the_day[0].to_menu_item}"
+        puts "#{DAYS[index]} dinner: #{meals_for_the_day[1].to_menu_item}"
         puts
       end
       puts "=" * MENU_CHARS_LENGTH
