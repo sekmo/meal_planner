4dc51c19c81e2a872d97e8e45d956e3b5c36e52e
sekmo <francescojjmari@gmail.com>
2022-05-07 17:01:57 +0200

    Refactor IngredientSampler#sample to avoid ingredient kind-specific methods

--- lib/meal_planner/ingredient_sampler.rb
+++ lib/meal_planner/ingredient_sampler.rb
@@ -9,67 +9,32 @@ module MealPlanner
     end
 
     def sample(ingredient_kind, current_meals)
-      if ingredient_kind == :carb
-        (carbs_that_respect_the_weekly_frequency_rule(current_meals) &
-          carbs_that_respect_the_last_2_meals_rule(current_meals)).sample
-      elsif ingredient_kind == :protein
-        (proteins_that_respect_the_weekly_frequency_rule(current_meals) &
-          proteins_that_respect_the_last_2_meals_rule(current_meals)).sample
-      else
-        (veggies_that_respect_the_weekly_frequency_rule(current_meals) &
-          veggies_that_respect_the_last_2_meals_rule(current_meals)).sample
-      end
+      (
+        ingredients_of_a_specific_kind_that_respect_the_weekly_frequency_rule(ingredient_kind, current_meals) &
+        ingredients_of_a_specific_kind_that_respect_the_last_2_meals_rule(ingredient_kind, current_meals)
+      ).sample
     end
 
     private
 
-    def ingredients_in_our_current_meals_by_kind(kind, current_meals)
-      current_meals.map(&kind).compact
-    end
-
-    def carbs_that_respect_the_weekly_frequency_rule(current_meals)
-      ingredients_book.carbs.select do |carb|
-        max_frequency_for_carb = max_weekly_ingredient_frequency.find { |rule| rule[:ingredient] == carb.name}&.fetch(:times)
-        !max_frequency_for_carb || ingredients_in_our_current_meals_by_kind(:carb, current_meals).count(carb) < max_frequency_for_carb
+    def ingredients_of_a_specific_kind_that_respect_the_weekly_frequency_rule(ingredient_kind, current_meals)
+      ingredients_book.ingredients_by_kind(ingredient_kind).select do |ingredient|
+        max_frequency_for_ingredient = max_weekly_ingredient_frequency.find { |rule| rule[:ingredient] == ingredient.name}&.fetch(:times)
+        !max_frequency_for_ingredient ||
+          ingredients_in_our_current_meals_by_kind(ingredient_kind, current_meals).count(ingredient) < max_frequency_for_ingredient
       end
     end
 
-    def proteins_that_respect_the_weekly_frequency_rule(current_meals)
-      ingredients_book.proteins.select do |protein|
-        max_frequency_for_protein = max_weekly_ingredient_frequency.find { |rule| rule[:ingredient] == protein.name}&.fetch(:times)
-        !max_frequency_for_protein || ingredients_in_our_current_meals_by_kind(:protein, current_meals).count(protein) < max_frequency_for_protein
-      end
+    def ingredients_in_our_current_meals_by_kind(ingredient_kind, current_meals)
+      current_meals.map(&ingredient_kind).compact
     end
 
-    def veggies_that_respect_the_weekly_frequency_rule(current_meals)
-      ingredients_book.veggies.select do |veggie|
-        max_frequency_for_veggie = max_weekly_ingredient_frequency.find { |rule| rule[:ingredient] == veggie.name}&.fetch(:times)
-        !max_frequency_for_veggie || ingredients_in_our_current_meals_by_kind(:veggie, current_meals).count(veggie) < max_frequency_for_veggie
-      end
+    def ingredients_of_a_specific_kind_that_respect_the_last_2_meals_rule(kind, current_meals)
+      ingredients_book.ingredients_by_kind(kind) - ingredients_of_a_specific_kind_in_our_last_2_meals(kind, current_meals)
     end
 
-    def carbs_in_our_last_2_meals(current_meals)
-      current_meals.last(2).map(&:carb).compact
-    end
-
-    def proteins_in_our_last_2_meals(current_meals)
-      current_meals.last(2).map(&:protein).compact
-    end
-
-    def veggies_in_our_last_2_meals(current_meals)
-      current_meals.last(2).map(&:veggie).compact
-    end
-
-    def carbs_that_respect_the_last_2_meals_rule(current_meals)
-      ingredients_book.carbs - carbs_in_our_last_2_meals(current_meals)
-    end
-
-    def proteins_that_respect_the_last_2_meals_rule(current_meals)
-      ingredients_book.proteins - proteins_in_our_last_2_meals(current_meals)
-    end
-
-    def veggies_that_respect_the_last_2_meals_rule(current_meals)
-      ingredients_book.veggies - veggies_in_our_last_2_meals(current_meals)
+    def ingredients_of_a_specific_kind_in_our_last_2_meals(kind, current_meals)
+      current_meals.last(2).map(&kind).compact
     end
   end
 end
